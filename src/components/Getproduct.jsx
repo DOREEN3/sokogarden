647f75b124c647c680247383cfaaccfeaaaeab5b
DOREEN3 <chorraymursla@gmail.com>
2025-10-14 12:17:54 +0300

    get products

--- src/components/Getproduct.jsx
+++ src/components/Getproduct.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import axios from 'axios'
+import React ,{useEffect,useState}from 'react'
 
 const Getproduct = () => {
+  // initialize three state
+  const [loading,setLoading]=useState("")
+  const [error,setError]=useState("")
+  const [products,setProducts]=useState([])
+  
+  // function to fetch products
+  const getproducts=async()=>{
+    setLoading("Please wait...")
+
+    try {
+      const response=await axios.get("https://doreen98.pythonanywhere.com/api/get_product_details")
+      setProducts(response.data)
+      setLoading("")
+    } catch (error) {
+      setError(error.message)
+      setLoading("")
+    }
+  }
+
+  useEffect(()=>{
+    getproducts()
+  },[])
+
+  // check if we have products
+  console.log(products)
   return (
-    <div>Getproduct</div>
+    <div className='row'>
+        <h3 className='text-primary text-center'>Available products</h3>
+        {/* map over our products and display them */}
+        {products.map(product=>(
+
+          <div className='col-md-3 justify-content-center mb-4'>
+          {/* card with equal sizes */}
+          <div className="card shadow card-margin">
+            {/* card image  */}
+            <img src="" alt="" />
+            <div className="card-body">
+              <h5>{product.product_name}</h5>
+              <h5>{product.product_category}</h5>
+              <h5>{product.product_cost}</h5>
+              <h5>{product.product_description}</h5>
+
+            </div>
+          </div>
+
+        </div>
+        ))}
+    </div>
   )
 }
 
