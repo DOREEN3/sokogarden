92de241662e50db16263eaf246c941d4d0cbc9a7
DOREEN3 <chorraymursla@gmail.com>
2025-10-21 12:06:46 +0300

    implementation of mpesa

--- src/components/Getproduct.jsx
+++ src/components/Getproduct.jsx
@@ -1,14 +1,18 @@
 import axios from 'axios'
 import React ,{useEffect,useState}from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Getproduct = () => {
+  let navigate=useNavigate() 
+
   // initialize three state
   const [loading,setLoading]=useState("")
   const [error,setError]=useState("")
   const [products,setProducts]=useState([])
   //initialize state for sorting
-  const [searchByField,setSearchField]=useState("")
-  const [sortOrder,setSordOrder]=useState("min")
+  const [searchTerm,setSearchTerm]=useState("")
+  const [sortOrder,setSordOrder]=useState("")
   
   // function to fetch products
   const getproducts=async()=>{
@@ -34,25 +38,25 @@ const Getproduct = () => {
 
   return (
     <div className='container-fluid'>
-      <h3 className='text-primary text-center p-4'>Available products</h3>
-      <div className="row mb-2">
+      <h3 className='text-primary text-center p-2'>Available products</h3>
+      <div className="row p-4">
         <div className="col-md-3">
-          <input type="text" placeholder='Search by name' value={searchByField} onChange={(e)=>setSearchField(e.target.value)} style={{height:"30px", width:"100%"}} />
+          <input type="text" placeholder='Search by name' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} style={{height:"35px", width:"100%" , padding:"10px"}} />
         </div>
         <div className="col-md-3">
-       <select name="category" id="category" className='w-100' style={{height:"30px"}}>
+       <select name="category" id="category" className='w-100' style={{height:"35px"}}>
         <option value="categories">All categories</option>
         <option value="product_name">Product Name</option>
         <option value="product_cost">Product Cost</option>
        </select>
         </div>
         <div className="col-md-2">
-          <select name="sortorder" id="sortorder" className='w-100' style={{height:"30px"}}>
+          <select name="min price" id="min price" value={sortOrder} className='w-100' style={{height:"35px"}}>
             <option value="product_cost">Min Price</option>
           </select>
         </div>
         <div className="col-md-2 " >
-          <select name="max" id="max" className='w-100' style={{height:"30px"}}>
+          <select name="max" id="max" value={sortOrder} className='w-100' style={{height:"35px"}}>
             <option value="product_cost" >Max Price</option>
           </select>
         </div>
@@ -76,7 +80,7 @@ const Getproduct = () => {
               
               <p><span className="fw-semibold">Product Description: </span>{product.product_description.slice(0,100)}</p>
               <h4 className='text-danger'><span className="fw-semibold">Price: </span> KSH.{product.product_cost}</h4>
-              <button className='btn btn-dark w-100 mt-2'>Purchase now</button>
+              <button onClick={()=>navigate("/mpesapayment", {state : {product}})} className='btn btn-dark w-100 mt-2'>Purchase now</button>
 
             </div>
           </div>
--- src/components/Mpesapayment.jsx
+++ src/components/Mpesapayment.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React ,{useState} from 'react'
+import { useLocation } from 'react-router-dom'
+
 
 const Mpesapayment = () => {
+  // we use uselocation to retrive our product 
+  const {product} =useLocation().state ||  {};
+  const imagepath='https://doreen98.pythonanywhere.com/static/images/'
+
+ 
+  const  [loading,setLoading]=useState("")
+  const [success,setSuccess]=useState('')
+  const [error,setError]=useState("")
+
+   // function to make payment 
+  const handleSubmit= async (e)=>{
+    e.preventDefault()
+    alert("My mpesa payment")
+
+  }
   return (
-    <div>Mpesapayment</div>
+    <div className='row justify-content-center mt-4'>
+      <div className="col-md-6 card p-4 shadow">
+        <h1 className='text-info text-center'>Lipa Na Mpesa</h1>
+      <img src={imagepath + product.product_photo} alt={product.product_name} className='mt-4 productimage' />
+      <div className="card-body  ">
+              <h4 className='text-primary fw-bold'>{product.product_name}</h4>
+              <h5 className='text-warning fst-italic'><span className="fw-semibold text-dark">Product Category: </span>{product.product_category}</h5>
+              
+              <p><span className="fw-semibold fs-5">Product Description: </span>{product.product_description.slice(0,100)}</p>
+              <h4 className='text-danger fs-5'><span className="fw-semibold">Price: </span> KSH.{product.product_cost}</h4>
+
+              <form onSubmit={handleSubmit}>
+                  <input type="tel" placeholder='Enter phone 254xxxxx' className='form-control fs-5' />
+              <button className='btn btn-dark w-100 mt-2'>Purchase now</button>
+              </form>
+
+            </div>
+      </div>
+    </div>
   )
 }
 
